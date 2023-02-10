4e63f7e1c22553342c723836daa00d565c0a439c
David Jandron <jandron1211@gmail.com>
2023-02-10 15:14:06 -0800

    WIP bugfix- not printing rates

--- src/index.js
+++ src/index.js
@@ -6,16 +6,28 @@ import Rates from './js/rates.js';
 
 //Business Logic
 
-function getExchange(currency1, currency2) {
-  Exchange.getExchange(currency1, currency2)
+function getExchange(currency, dollar) {
+  Exchange.getExchange(currency, dollar)
   .then(function (response) {
     if (response) {
-      printElements(response, currency1, currency2);
+      printElements(response, currency, dollar);
     } else {
-      printError(response, currency1, currency2);
+      printError(response, currency, dollar);
     }
   }); 
 }
+
+function getRate() {
+  Rates.getRate()
+  .then(function (response) {
+    if (response) {
+      printElements(response);
+    } else {
+      printError(response);
+    }
+  });
+}
+
 
 function printRates(data) {
    document.getElementById("CAD").innerText = 'CAD: ${data.conversion_rates.CAD}';
@@ -28,44 +40,34 @@ function printRates(data) {
    document.getElementById("GBP").innerText = 'GBP: ${data.conversion_rates.GBP}';
 }
 
-// function getRate() {
-//   Rates.getRate()
-//   .then(function ("") {
-//     if ("") {
-//       printRates("", (""));
-//     } else {
-//       printError("", ());
-//     }
-//   });
-// }
-//response4
 
 // UI
 
-function printError(request, search) {
-  document.querySelector('output').innerText = `There was an error accessing currency exchange rates ${search}: ${request.status} ${request.statusText}`;
+function printRatesError(errors) {
+  document.querySelector('rates-error').innerText = `There was an error accessing currency exchange rates ${search}: ${request.status} ${request.statusText}`;
 }
 
 function printElements(data) {
   document.getElementById("output").innerText = `That will get you ${data[0].conversion_result.toFixed(2)} in ${data[0].target_code}`;
 }
 
-// function printError(noInfo) {
-//   if (noInfo[0].status === 404) {
-//     document.getElementById("output").innerText = "No such currency.";
-//   } else {
-//     document.getElementById("output").innerText = `There was an error in currency conversion: ${noInfo[0].status} ${noInfo[1].result}`;
-//   }
-// }
+ function printError(noInfo) {
+   if (noInfo[0].status === 404) {
+     document.getElementById("output").innerText = "No such currency.";
+   } else {
+     document.getElementById("output").innerText = `There was an error in currency conversion: ${noInfo[0].status} ${noInfo[1].result}`;
+   }
+ }
 
 function handleFormSubmission(event) {
   event.preventDefault();
-  const currency1 = document.getElementById("currency1").value;
-  const currency2 = document.getElementById("currency2").value;
-  getExchange(currency1, currency2);
+  const dollar = document.getElementById("dollar").value;
+  const currency = document.getElementById("currency").value;
+  getExchange(dollar, currency);
 }
 
 window.addEventListener("load", function() {
-  document.getElementById("input-form").addEventHandler("submit", handleFormSubmission);
+  document.getElementById("input-form").addEventListener("submit", handleFormSubmission);
+  this.document.getElementById("rates").addEventListener("click", getRates);
 });
 
--- src/js/exchange.js
+++ src/js/exchange.js
@@ -1,7 +1,7 @@
 export class Exchange {
 
-  static getExchange(currency1, currency2) {
-    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/USD/${currency1}/${currency2}`)
+  static getExchange(currency, dollar) {
+    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/USD/${currency}/${dollar}`)
       .then(function(response) {
         if (!response.ok) {
           const errorMessage = `${response.status} ${response.statusText}`;
