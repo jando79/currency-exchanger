2ace234dab584a3dbc82ae20c371fa3cd22e1d7c
David Jandron <jandron1211@gmail.com>
2023-02-10 14:39:05 -0800

    ADD functionality in index.js, changed format to USD text box to country drop down for ease

--- src/index.js
+++ src/index.js
@@ -18,15 +18,14 @@ function getExchange(currency1, currency2) {
 }
 
 function printRates(data) {
-  document.getElementById("USD").innerText = 'USD: ${data.conversion_rates.USD}';
-  document.getElementById("CAD").innerText = 'CAD: ${data.conversion_rates.CAD}';
-  document.getElementById("MXN").innerText = 'MXN: ${data.conversion_rates.MXN}';
-  document.getElementById("BRL").innerText = 'BRL: ${data.conversion_rates.BRL}';
-  document.getElementById("CNY").innerText = 'CNY: ${data.conversion_rates.CNY}';
-  document.getElementById("JPY").innerText = 'JPY: ${data.conversion_rates.JPY}';
-  document.getElementById("RUB").innerText = 'RUB: ${data.conversion_rates.RUB}';
-  document.getElementById("EUR").innerText = 'EUR: ${data.conversion_rates.EUR}';
-  document.getElementById("GBP").innerText = 'GBP: ${data.conversion_rates.GBP}';
+   document.getElementById("CAD").innerText = 'CAD: ${data.conversion_rates.CAD}';
+   document.getElementById("MXN").innerText = 'MXN: ${data.conversion_rates.MXN}';
+   document.getElementById("BRL").innerText = 'BRL: ${data.conversion_rates.BRL}';
+   document.getElementById("CNY").innerText = 'CNY: ${data.conversion_rates.CNY}';
+   document.getElementById("JPY").innerText = 'JPY: ${data.conversion_rates.JPY}';
+   document.getElementById("RUB").innerText = 'RUB: ${data.conversion_rates.RUB}';
+   document.getElementById("EUR").innerText = 'EUR: ${data.conversion_rates.EUR}';
+   document.getElementById("GBP").innerText = 'GBP: ${data.conversion_rates.GBP}';
 }
 
 // function getRate() {
@@ -44,6 +43,29 @@ function printRates(data) {
 // UI
 
 function printError(request, search) {
-  document.querySelector('showResult').innerText = `There was an error accessing cuisines ${search}: ${request.status} ${request.statusText}`;
+  document.querySelector('output').innerText = `There was an error accessing currency exchange rates ${search}: ${request.status} ${request.statusText}`;
 }
+
+function printElements(data) {
+  document.getElementById("output").innerText = `That will get you ${data[0].conversion_result.toFixed(2)} in ${data[0].target_code}`;
+}
+
+// function printError(noInfo) {
+//   if (noInfo[0].status === 404) {
+//     document.getElementById("output").innerText = "No such currency.";
+//   } else {
+//     document.getElementById("output").innerText = `There was an error in currency conversion: ${noInfo[0].status} ${noInfo[1].result}`;
+//   }
+// }
+
+function handleFormSubmission(event) {
+  event.preventDefault();
+  const currency1 = document.getElementById("currency1").value;
+  const currency2 = document.getElementById("currency2").value;
+  getExchange(currency1, currency2);
+}
+
+window.addEventListener("load", function() {
+  document.getElementById("input-form").addEventHandler("submit", handleFormSubmission);
+});
 
