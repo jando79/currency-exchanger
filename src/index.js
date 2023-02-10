3f6636adf415508925826db0782dbf46c1d0fb47
David Jandron <jandron1211@gmail.com>
2023-02-10 11:05:03 -0800

    WIP code in index.js, researching functionality

--- src/index.js
+++ src/index.js
@@ -1,4 +1,31 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import Exchange from './js/exchange.js';
+import Rates from './js/rates.js';
 
+//Business Logic
+
+function getExchange(currency, amount) {
+  Exchange.getExchange(currency, amount)
+  .then(function ("") {
+    if ("") {
+      printElements("", currency, amount);
+    } else {
+      printError("", currency, amount);
+    }
+  }); 
+}
+//response1
+
+function getRate() {
+  Rates.getRate()
+  .then(function ("") {
+    if ("") {
+      printRates("", (""));
+    } else {
+      printError("", ());
+    }
+  });
+}
+//response4
