a4ae3ca4e497ab4ad0146c70a8cd35fa2271edaf
David Jandron <jandron1211@gmail.com>
2023-02-10 13:53:19 -0800

    ADD function to print conversion results, also changed select menu for country currency selection

--- src/index.js
+++ src/index.js
@@ -6,28 +6,39 @@ import Rates from './js/rates.js';
 
 //Business Logic
 
-function getExchange(currency, amount) {
-  Exchange.getExchange(currency, amount)
-  .then(function ("") {
-    if ("") {
-      printElements("", currency, amount);
+function getExchange(currency1, currency2) {
+  Exchange.getExchange(currency1, currency2)
+  .then(function (response) {
+    if (response) {
+      printElements(response, currency1, currency2);
     } else {
-      printError("", currency, amount);
+      printError(response, currency1, currency2);
     }
   }); 
 }
-//response1
 
-function getRate() {
-  Rates.getRate()
-  .then(function ("") {
-    if ("") {
-      printRates("", (""));
-    } else {
-      printError("", ());
-    }
-  });
+function printRates(data) {
+  document.getElementById("USD").innerText = 'USD: ${data.conversion_rates.USD}';
+  document.getElementById("CAD").innerText = 'CAD: ${data.conversion_rates.CAD}';
+  document.getElementById("MXN").innerText = 'MXN: ${data.conversion_rates.MXN}';
+  document.getElementById("BRL").innerText = 'BRL: ${data.conversion_rates.BRL}';
+  document.getElementById("CNY").innerText = 'CNY: ${data.conversion_rates.CNY}';
+  document.getElementById("JPY").innerText = 'JPY: ${data.conversion_rates.JPY}';
+  document.getElementById("RUB").innerText = 'RUB: ${data.conversion_rates.RUB}';
+  document.getElementById("EUR").innerText = 'EUR: ${data.conversion_rates.EUR}';
+  document.getElementById("GBP").innerText = 'GBP: ${data.conversion_rates.GBP}';
 }
+
+// function getRate() {
+//   Rates.getRate()
+//   .then(function ("") {
+//     if ("") {
+//       printRates("", (""));
+//     } else {
+//       printError("", ());
+//     }
+//   });
+// }
 //response4
 
 // UI
--- src/js/exchange.js
+++ src/js/exchange.js
@@ -1,7 +1,7 @@
 export class Exchange {
 
-  static getExchange(currency, amount) {
-    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/USD/${currency}/${amount}`)
+  static getExchange(currency1, currency2) {
+    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/USD/${currency1}/${currency2}`)
       .then(function(response) {
         if (!response.ok) {
           const errorMessage = `${response.status} ${response.statusText}`;
